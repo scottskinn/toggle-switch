73b94193d8eba638f9bfe76575c2f5c34385bb06
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-05-04 15:48:00 -0400

    random jokes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Component
+} from "react";
+import ToggleSwitch from "./components/toggle-switch";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      jokes: ""
+    }
+  }
+
+  async componentDidMount() {
+    const url = "https://icanhazdadjoke.com/"
+    let result = null;
+    try {
+      result = await axios.get(url, {
+        headers: {
+          Accept: "application/json"
+        }
+      })
+    } catch(e) {
+      console.log(e);
+    }
+    this.setState({jokes: result.data.joke});
+    console.log(this.state.jokes);
+    // setJoke(`${this.state.jokes}`);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <div className="Joke">
+          {this.state.jokes}
+          </div>
+
+          <button onClick={this.state.jokes} className="Toggle" type="button">
+            Jokes
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/toggle-switch.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+
+const ToggleSwitch = (props) => {
+  
+  return ( 
+    
+    <button className="Toggle" onClick={props.onClick}>New Jokes</button> 
+    
+  ); 
+  
+} 
+
+export default ToggleSwitch;
