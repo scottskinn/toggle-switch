da57b106c2d4e08f8267b1b52f3d2e747aa43760
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-05-23 16:03:37 -0400

    toggle joke

--- src/App.js
+++ src/App.js
@@ -1,52 +1,10 @@
-import React, {
-  Component
-} from "react";
-import ToggleSwitch from "./components/toggle-switch";
+import Toggle from "./components/toggle-switch.js";
 import "./App.css";
-import axios from "axios";
 
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      jokes: ""
-    }
-  }
-
-  async componentDidMount() {
-    const url = "https://icanhazdadjoke.com/"
-    let result = null;
-    try {
-      result = await axios.get(url, {
-        headers: {
-          Accept: "application/json"
-        }
-      })
-    } catch(e) {
-      console.log(e);
-    }
-    this.setState({jokes: result.data.joke});
-    console.log(this.state.jokes);
-    // setJoke(`${this.state.jokes}`);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="App">
-          <div className="Joke">
-          {this.state.jokes}
-          </div>
-
-          <button onClick={this.state.jokes} className="Toggle" type="button">
-            Jokes
-          </button>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
-
-export default App;
+export default function App() {
+  return (
+    <header>
+      <Toggle />
+    </header>
+  );
+};
--- src/components/toggle-switch.js
+++ src/components/toggle-switch.js
@@ -1,14 +1,73 @@
-import React from 'react'; 
+import React from 'react';
+import axios from "axios";
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+    this.jokes = "https://icanhazdadjoke.com/"
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  async componentDidMount() {
+    const url = "https://icanhazdadjoke.com/"
+    let result = null;
+    try {
+      result = await axios.get(url, {
+        headers: {
+          Accept: "application/json"
+        }
+      })
+    } catch(e) {
+      console.log(e);
+    }
+    this.setState({jokes: result.data.joke});
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+        
+        {this.state.isToggleOn && <p> {this.state.jokes} </p>}
+      </div>
+    );
+  }
+}
+
+export default Toggle;
 
 
-const ToggleSwitch = (props) => {
-  
-  return ( 
-    
-    <button className="Toggle" onClick={props.onClick}>New Jokes</button> 
-    
-  ); 
-  
-} 
 
-export default ToggleSwitch;
+
+// class Toggle extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state={
+//       jokes: "https://icanhazdadjoke.com/"
+      
+//     }
+//     // console.log(props)
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button>ON</button>
+//         <h3>Joke: {this.state.jokes}</h3>
+//       </div>
+//     );
+//   }
+// };
+
+// // change create Root back to render
+// const root = ReactDOM.render(document.getElementById('root'));
+// root.render(<Toggle />);
